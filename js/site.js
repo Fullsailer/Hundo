2e0bb431021cd38ae98c400aefaba4a4d3a4c449
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2021-08-03 16:36:45 -0400

    Added JS logic for numbers
    Added JS for Hundo to display number and to bold even numbers.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,64 @@
-//Calls Hello World
-function HelloWorld() {
-    alert("Hello Worls");
+// get the values from the page
+//starts of controller function
+function getValues() {
+    //get values from the page
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+
+    //We need to validate our input
+    //parse into Intergers
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+        //we call generateNumbers
+        let numbers = generateNumbers(startValue, endValue);
+        //we call displayNumbers
+        displayNumber(numbers);
+
+    } else {
+        alert("You must enter integers.");
+    }
+
+}
+
+//generate numbers form startvalue to the endValue
+//logic function(s)
+function generateNumbers(sValue, eValue) {
+
+    let numbers = [];
+
+    //we want to get all numbers from start to end
+    for (let index = sValue; index <= eValue; index++) {
+
+        //this will execute in a lopp until index = eValue
+        numbers.push(index);
+    }
+
+
+    return numbers;
+}
+
+//display the numbers and mark even numbers bold
+//display or view functions
+function displayNumber(numbers) {
+
+    let templateRows = "";
+    for (let index = 0; index < numbers.length; index++) {
+
+        let className = "even";
+
+        let numbers = numbers[index];
+
+        if (number % 2 == 0) {
+            className = "even";
+        } else {
+            className = "odd";
+        }
+
+        templateRows += `<tr><td class="${className}" >${number}</td></tr>`;
+
+    }
+
+    document.getElementById("result").innerHTML = templateRows;
 }
